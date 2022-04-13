75208a8a57b31cffc3eb0fe7147b62d0502dd5be
Sandipan Chakraborty <chak.sandipan22@gmail.com>
2022-04-14 03:53:20 +0530

    25th commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return "HELLO WORLD by SANDIPAN v2 "
+    return "HELLO WORLD by SANDIPAN v2 using tag "
 
 if __name__=="__main__":
     app.run()
